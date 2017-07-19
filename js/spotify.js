56be73631f739cb16c63559280704b6061998eeb
criscarguz <criscarguz@gmail.com>
2017-07-19 12:44:43 +0200

    sadFace

--- js/spotify.js
+++ js/spotify.js
@@ -5,13 +5,11 @@ var $artist = $('#artist');
 
 $btnSearch.on('click', function(){
 
-	$artist.get('https://api.spotify.com/v1/search?type=artist&query=SEARCHTERM', {}, searchArtist);
 
 });
-
 //Search artist
 
 function searchArtist(){
 
-	
+console.log('me')
 }
